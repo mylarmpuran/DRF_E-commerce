02f6a4d3290f9fec65edebb48da0965c267e6b3e
puran kumar <mylarmpuran@gmail.com>
2025-05-01 22:28:42 +0530

    "views"

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -42,11 +42,116 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OrderItemSerializer(serializers.ModelSerializer):
-
      
     class Meta:
         model = OrderItem
         field = '__all__'
 
 
+class CartSerializer(serializers.ModelSerializer):
+    items = ProductSerializer(many=True, read_only = True)
 
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class ShippingSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+class CouponSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Coupon
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    customer = UserSerializer(read_only = True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class WishlistSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = Wishlist
+        fields = '__all__'
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    
+
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+class BlogSerializer(serializers.ModelSerializer):
+    auther = UserSerializer(read_only = True)
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class ContactSerializer(serializers.ModelSerializer):
+    
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+class FAQSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = FAQ
+        fields = '__all__'
+
+
+class AnalyticsSerializer(serializers.ModelSerializer):
+    popular_products = ProductSerializer(many=True, read_only = True)
+    class Meta:
+        model = Analytics
+        fields = '__all__'
+
+class ConfigurationSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Analytics
+        fields = '__all__'
+
+class TaxSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Analytics
+        fields = '__all__'
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+class RefundSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only = True)
+    
+    class Meta:
+        model = Refund
+        fields = '__all__'
